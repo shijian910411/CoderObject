3459973abb974b3e57b5d9120271689ddef91f93
石坚 <shijian@shijiandeMacBook-Pro.local>
2015-10-28 16:13:55 +0800

    first commit

---
+++ TestRuntime/RuntimeObj.h
@@ -0,0 +1,13 @@
+//
+//  RuntimeObj.h
+//  TestRuntime
+//
+//  Created by 石坚 on 15/10/28.
+//  Copyright © 2015年 石坚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RuntimeObj : NSObject
+
+@end
---
+++ TestRuntime/RuntimeObj.m
@@ -0,0 +1,13 @@
+//
+//  RuntimeObj.m
+//  TestRuntime
+//
+//  Created by 石坚 on 15/10/28.
+//  Copyright © 2015年 石坚. All rights reserved.
+//
+
+#import "RuntimeObj.h"
+
+@implementation RuntimeObj
+
+@end
